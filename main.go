4301dc2a53ab15faa55569a982709390c0115c72
artem <noobkesha@gmail.com>
2025-10-24 01:08:43 +0300

    dev: fix: fixed paths and added wrapper

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
-	"first-go/scr/codewars"
+	"first-go/src/codewars"
+	"fmt"
 )
 
 func main() {
+	DEBUG := true
 	//roadmap.Base()
-	codewars.EvenOrOdd(133)
-	codewars.RowSumOddNumbers(5)
-	codewars.CleverRowSumOddNumbers(5)
+	callAllFunctions(DEBUG)
 
+}
+
+func callAllFunctions(debug bool) {
+	if !debug {
+		codewars.EvenOrOdd(133)
+		codewars.RowSumOddNumbers(5)
+		codewars.CleverRowSumOddNumbers(5)
+
+		return
+	}
+
+	fmt.Printf("codewars.EvenOrOdd(133) %s \n", codewars.EvenOrOdd(133))
+	fmt.Printf("codewars.RowSumOddNumbers(5) %d \n", codewars.RowSumOddNumbers(5))
+	fmt.Printf("codewars.CleverRowSumOddNumbers(5) %d \n", codewars.CleverRowSumOddNumbers(5))
 }
